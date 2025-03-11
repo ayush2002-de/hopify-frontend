2f96945923a9b85833f9d1aebdf34fbee1e4c3fc
ayush.c <ayush.c@juspay.in>
2025-03-11 19:59:46 +0530

    add click functionality

--- src/components/Hospital.tsx
+++ src/components/Hospital.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import image from "../assets/hospital1.jpg"
+import { useState } from "react";
 
 interface CartItem {
   id: number;
@@ -9,19 +10,25 @@ interface CartItem {
   link: string;
 }
 
-const cartItems: CartItem[] = [
-  { id: 1, image: image, title: "Apollo", description: "Hospital", link: "/hospital/apollo" },
-  { id: 2, image: image, title: "Apollo", description: "Hospital", link: "/hospital/apollo" },
-  { id: 3, image: image, title: "Apollo", description: "Hospital", link: "/hospital/apollo" },
-  { id: 4, image: image, title: "Apollo", description: "Hospital", link: "/hospital/apollo" },
+const cartItems: CartItem [] = [
+  { id: 1, image: image, title: "Apollo1", description: "Hospital", link: "/hospital/apollo" },
+  { id: 2, image: image, title: "Apollo2", description: "Hospital", link: "/hospital/apollo" },
+  { id: 3, image: image, title: "Apollo3", description: "Hospital", link: "/hospital/apollo" },
+  { id: 4, image: image, title: "Apollo4", description: "Hospital", link: "/hospital/apollo" },
+  { id: 5, image: image, title: "Apollo5", description: "Hospital", link: "/hospital/apollo" },
+  { id: 6, image: image, title: "Apollo6", description: "Hospital", link: "/hospital/apollo" },
+  { id: 7, image: image, title: "Apollo7", description: "Hospital", link: "/hospital/apollo" },
+  { id: 8, image: image, title: "Apollo8", description: "Hospital", link: "/hospital/apollo" },
 ];
 
+
 const ImageCart = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
   const navigate = useNavigate();
 
   return (
 <div className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10 max-w-6xl">
-  {cartItems.map((item) => (
+  {cartItems.slice(currentIndex, currentIndex + 4).map((item) => (
     <div
       key={item.id}
       className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer transition transform hover:scale-105"
@@ -35,10 +42,13 @@ const ImageCart = () => {
     </div>
   ))}
 
-<div className="absolute bottom-[-20px] items-center justify-end z-2">
+<div className="absolute inset-y-35 right-0 z-2 hover:scale-105">
 <button
       type="button"
       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      onClick={() => {if (currentIndex <= cartItems.length - 4) {
+        setCurrentIndex(prev => prev + 1)
+      }}}
     >
       <svg
         className="w-4 h-4"
